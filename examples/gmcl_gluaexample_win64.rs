16b0ae39ce81afd9235898cdc589a4568cd166b2
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2022-01-15 13:57:21 +0000

    build: Rename gm_example to gmcl/win64 for easier testing.

--- examples/gmcl_gluaexample_win64.rs
+++ examples/gmcl_gluaexample_win64.rs
@@ -5,8 +5,8 @@ extern crate glua;
 
 use gmod::gmod13_open;
 use gmod::lua_string;
-use glua::{State, ExtendedLuaState};
-use glua::stdlib::enums::{FORCE, HasLuaGlobal};
+use glua::{State};
+use glua::stdlib::enums::{FORCE};
 use glua::stdlib::globals::Globals;
 
 
@@ -15,13 +15,9 @@ unsafe fn gmod13_open(lua: State) -> i32 {
     // Make a new table.
     lua.new_table();
     let tab_ref = lua.reference();
-    lua.from_reference(tab_ref);
-
-    // The key which we'll be filling.
-    lua.push_string("test_key");
 
     // Call AccessorFunc
-    lua.AccessorFunc(-2, -1, "test_func", Some(FORCE::STRING));
+    lua.AccessorFunc(tab_ref, "test_key", "test_func", FORCE::STRING);
 
     // Finally, set our table in _G
     lua.from_reference(tab_ref);
